e0d34d98cf2cc344f71eb5efb4aa8c0443a27561
Hà Tiến Sáng <bemimnitran@gmail.com>
2025-09-01 10:38:50 +0700

    Update HakoMonetTheme.user.js

--- HakoMonetTheme.user.js
+++ HakoMonetTheme.user.js
@@ -1,2 +1,33 @@
+// ==UserScript==
+// @name         Hako: Monet Theme
+// @namespace    https://github.com/sang765
+// @version      2.9.5
+// @description  Material You theme for Hako/DocLN.
+// @description:vi Material You theme dành cho Hako/DocLN.
+// @icon         https://docln.sbs/img/logo-9.png
+// @author       SangsDayy
+// @match        https://docln.sbs/*
+// @match        https://docln.net/*
+// @match        https://ln.hako.vn/*
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @grant        GM_xmlhttpRequest
+// @connect      *
+// @run-at       document-end
+// @require      https://greasyfork.org/scripts/447115-gm-config/code/GM_config.js?version=1060849
+// @resource     mainJS ./main.js
+// @resource     monetJS ./module/monet.js
+// @resource     crosUnblockJS ./module/cros-unblock.js
+// @resource     infoTruyenJS ./class/info-truyen.js
+// @resource     animationJS ./class/animation.js
+// @resource     monetClassJS ./class/monet.js
+// @resource     tagColorJS ./class/tag-color.js
+// ==/UserScript==
 
-
+(function() {
+    'use strict';
+    
+    // Load main.js
+    const mainJS = GM_getResourceText('mainJS');
+    eval(mainJS);
+})();
