63a0dce935174e235f1d8fce94987418bb279e63
Hà Tiến Sáng <bemimnitran@gmail.com>
2025-09-02 08:11:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,7 @@
     
     const DEBUG = true;
     const CHECK_UPDATE_INTERVAL = 30 * 60 * 1000; // 30 phút
-    const VERSION = 'N20250901';
+    const VERSION = '2.9.7';
     const GITHUB_RAW_URL = 'https://raw.githubusercontent.com/sang765/HakoMonetTheme/nightly/HakoMonetTheme.user.js';
     
     function debugLog(...args) {
