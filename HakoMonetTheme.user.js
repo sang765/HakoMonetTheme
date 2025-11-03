f5b497fa48e92147b2953e16325d981eedddb99a
Hà Tiến Sáng <bemimnitran@gmail.com>
2025-11-03 23:20:07 +0700

    Update HakoMonetTheme.user.js

--- HakoMonetTheme.user.js
+++ HakoMonetTheme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hako: Monet Theme
 // @namespace    https://github.com/sang765
-// @version      4.2.8
+// @version      4.2.9
 // @description  Material You theme for Hako/DocLN.
 // @description:vi Material You theme dành cho Hako/DocLN.
 // @icon         https://raw.githubusercontent.com/sang765/HakoMonetTheme/main/.github/assets/logo.png
