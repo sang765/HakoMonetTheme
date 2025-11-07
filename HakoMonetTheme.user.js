dc28450b93adfaeb3299baba2a52d43ff76f8e32
sang765 <bemimnitran@gmail.com>
2025-11-07 15:01:03 +0700

    chore: bump version to 5.0.8

--- HakoMonetTheme.user.js
+++ HakoMonetTheme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hako: Monet Theme
 // @namespace    https://github.com/sang765
-// @version      5.0.7
+// @version      5.0.8
 // @description  Material You theme for Hako/DocLN.
 // @description:vi Material You theme dành cho Hako/DocLN.
 // @icon         https://raw.githubusercontent.com/sang765/HakoMonetTheme/main/.github/assets/logo.png
